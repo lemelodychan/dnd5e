7c9827c181f5851a614d1e44032957e71ee51f73
Melody <elodie.dassance@gmail.com>
2024-03-23 12:13:20 +0900

    Update page.js

--- src/app/characters/[id]/page.js
+++ src/app/characters/[id]/page.js
@@ -15,7 +15,6 @@ export async function generateStaticParams() {
 export default async function CharacterPage({ params: { id } }) {
     const { data: character, error: characterError } = await supabase.from('Characters').select().match({ id }).single();
     if (characterError) {
-        console.error('Error fetching character:', characterError.message);
         return <div>Error fetching character</div>;
     }
     if (!character) {
@@ -25,15 +24,12 @@ export default async function CharacterPage({ params: { id } }) {
     // FETCH CHARACTER CLASS
     const { data: characterClasses, error: classError } = await supabase.from('Class').select().eq('name', character.class);
     if (classError) {
-        console.error('Error fetching character class:', classError.message);
         return <div>Error fetching character class</div>;
     }
     if (!characterClasses || characterClasses.length === 0) {
-        console.error(`No class found with name "${character.class}"`);
         return <div>No class found with name "{character.class}"</div>;
     }
     if (characterClasses.length > 1) {
-        console.error(`Multiple classes found with name "${character.class}"`);
         return <div>Multiple classes found with name "{character.class}"</div>;
     }
 
